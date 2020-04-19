fa74e60ee9b09bf7fc7fbf612fd8ae7029f87867
SDchao <wentaochens@outlook.com>
2020-04-19 20:58:41 +0800

    Plugins Changde

--- bot.py
+++ bot.py
@@ -3,9 +3,7 @@ from os import path
 import nonebot
 import config
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     nonebot.init(config)
-    # nonebot.load_plugins(
-    #     path.join(path.dirname(__file__),"sdchaos","plugins"),"sdchaos.plugins"
-    #     )
+    nonebot.load_plugin("sdchaos.plugin.nmsl")
     nonebot.run(host="127.0.0.1", port=8081)
