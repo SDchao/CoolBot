951fa9e92e265de470405c03bd1be6d8c12ff740
SDchao <wentaochens@outlook.com>
2020-04-24 12:28:45 +0800

    Add nmsl keyword

--- sdchaos/plugins/nmsl/__init__.py
+++ sdchaos/plugins/nmsl/__init__.py
@@ -9,6 +9,6 @@ async def _(session : nonebot.CommandSession):
         reply_msg = get_new_line()
         await session.send(reply_msg)
 
-@nonebot.on_natural_language(keywords=["骂","对线","嘴臭","祖安","罗东旭","ldx"])
+@nonebot.on_natural_language(keywords=["骂","对线","嘴臭","祖安","罗东旭","ldx", "东旭"])
 async def _(session : nonebot.NLPSession):
     return nonebot.IntentCommand(90.0,"nmsl")
